e8ac0081a0d44daa15a5d3a52da84f4e11a1ba03
ci-build <ci-build@disfactory.tw>
2020-02-26 14:54:55 +0000

    Deploy [ci skip]

---
+++ precache-manifest.41d4977e870d2828f6cc2b491e3aa04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2763efaf4e5fb589e4",
+    "url": "/css/app.4249b188.css"
+  },
+  {
+    "revision": "f0d236385589845d2ea7",
+    "url": "/css/chunk-vendors.cebb0e73.css"
+  },
+  {
+    "revision": "d7134ed4f942ce0e024b3ce86478dc1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a2763efaf4e5fb589e4",
+    "url": "/js/app-legacy.eda18031.js"
+  },
+  {
+    "revision": "f0d236385589845d2ea7",
+    "url": "/js/chunk-vendors-legacy.8b4382ba.js"
+  },
+  {
+    "revision": "50601b136bb3d20b9f228c0eff96f834",
+    "url": "/manifest.json"
+  }
+]);
